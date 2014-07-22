64a9db752001f7eb978fc4860d0503f14701befa
mordauk <mordauk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-22 14:11:31 +0000

    Tag 1.1.5
    git-svn-id: https://plugins.svn.wordpress.org/easy-digital-downloads-csv-manager/tags/1.1.5@952858 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class.payment-history-importer.php
+++ includes/class.payment-history-importer.php
@@ -429,7 +429,7 @@ if( !class_exists( 'EDD_CSV_Payment_History_Importer' ) ) {
                         )
                     );
 
-                    $user    = get_user_by( 'id', $user_id );
+                    $user    = get_userdata( $user_id );
                     $email   = $user->user_email;
                 } else {
                     $user_id = $user->ID;
@@ -439,7 +439,7 @@ if( !class_exists( 'EDD_CSV_Payment_History_Importer' ) ) {
                 $download_errors = array();
 
                 // Setup downloads
-                if( $downloads_key && !empty( $new_row[ $downloads_key ] ) ) {
+                if( false !== $downloads_key && !empty( $new_row[ $downloads_key ] ) ) {
 
                     $downloads       = array_map( 'trim', explode( '|', $new_row[ $downloads_key ] ) );
                     $final_downloads = array();
